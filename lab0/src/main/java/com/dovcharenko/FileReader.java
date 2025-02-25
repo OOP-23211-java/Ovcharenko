d6501e28cf9279ab997bb868b3feaddbf8bdd98f
dadashasha <dararenko@gmail.com>
2025-02-25 15:15:59 +0700

    Оптимизированно использование BufferedReader и добавлена проверка на открытие файла

--- lab0/src/main/java/com/dovcharenko/FileReader.java
+++ lab0/src/main/java/com/dovcharenko/FileReader.java
@@ -5,15 +5,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.util.Arrays;
 
 public class FileReader {
-    public static List<String> readFileFromResources(String fileName) throws IOException {
-        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-            return reader.lines().toList();
-        } catch (Exception e) {
-            throw new IOException(e);
+    public static long readFileFromResources(String fileName) throws IOException {
+        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName)) {
+            if (inputStream == null) {
+                throw new IOException("Файл не найден: " + fileName);
+            }
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                return reader.lines()
+                        .flatMap(line -> Arrays.stream(line.split("\\s+")))
+                        .filter(word -> !word.isEmpty())
+                        .count();
+            }
         }
     }
 }
